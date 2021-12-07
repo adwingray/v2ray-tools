69f8724b80a789e1410f84dcf26965e89bc98ed5
AdwinKing <jin2000112014@gmail.com>
2021-12-07 14:49:36 +0800

    use poetry to manager deps

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = " v2t - for updating v2ray subscription and choosing node";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        app = pkgs.poetry2nix.mkPoetryApplication {
+          projectDir = ./.;
+        };
+
+        packageName = "v2t";
+      in {
+        packages.${packageName} = app;
+
+        defaultPackage = self.packages.${system}.${packageName};
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ poetry ];
+          inputsFrom = builtins.attrValues self.packages.${system};
+        };
+      });
+}
